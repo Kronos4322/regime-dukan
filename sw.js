81be34ff91c4f79d2b617d3c64f056cea0f4bd7a
Camille <camille.chapuis43@gmail.com>
2026-07-17 13:32:19 +0200

    Application de suivi du régime Dukan (PWA)

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "dukan-app-v2";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-180.png"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Cache-first pour l'app shell, réseau sinon (avec repli sur le cache si hors-ligne)
+self.addEventListener("fetch", e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      const fetchPromise = fetch(e.request).then(res=>{
+        if(res && res.status===200){
+          const copy=res.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached || fetchPromise;
+    })
+  );
+});
